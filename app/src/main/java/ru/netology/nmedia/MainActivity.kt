641a87c832426cba0e92a875abca5976cf2b1183
Rita R <prostorita2013.p@yandex.ru>
2023-06-13 23:56:06 +0300

    RecyclerView

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,7 +3,7 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import ru.netology.nmedia.functions.formatNumber
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
@@ -11,31 +11,21 @@ import ru.netology.nmedia.viewmodel.PostViewModel
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val viewModel by viewModels<PostViewModel>()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                author.text = post.author
-                published.text = post.published
-                content.text = post.content
-                like.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
-                )
-                countLikes.text = formatNumber(post.likes)
-                countReposts.text = formatNumber(post.reposts)
+        val adapter = PostsAdapter({
+            viewModel.likeById(it.id)
+        },
+            { viewModel.repostById(it.id) }
+        )
+
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
 
 
-            }
-        }
-        binding.like.setOnClickListener {
-
-            viewModel.like()
-        }
-        binding.repost.setOnClickListener {
-            viewModel.repost()
         }
     }
 
